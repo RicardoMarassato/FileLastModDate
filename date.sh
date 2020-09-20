37dc1b1de79646a3047b8707e6cf2a36f6abe5c0
RicardoMarassato <ricardo.marassato@gmail.com>
2020-09-20 16:45:12 -0300

    comments added

--- date.sh
+++ date.sh
@@ -1,14 +1,10 @@
-#File update check in Unix Epoch time format.
-
-#Developed by: Ricardo Marassato.
-#Script to check if a file is beeing updated on a certain quantity of days.
-#Check https://www.epochconverter.com/ to learn more about Unix epoch.
-
 #adate - Get the current date of a file on Unix Epoch time format.
 #sdate - Current system  date on Unix Epoch time format.
 #olddate - System date 10 days before (Quantity of days can be changed to fill your needs) on Unix Epoch time format.
 
-adate=$(date +%s -r /media/ricardo/Data/Dados/Linux/Backup/Coding/Soap/test.py) 
+#Script returns 1 if the file was modified in the last 10 days.
+
+adate=$(date +%s -r /FILE_PATH) 
 sdate=$(date +%s)		  					
 olddate=$(date +%s -d "-10 Days") 					
 
